6cda9483877ed0dc007bf4a299e75290d0bb8cec
milliorn <scottmilliorn@gmail.com>
2023-11-07 19:48:45 -0800

    Search Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-function App() {
+import { useRef } from "react";
+import { Form } from "react-bootstrap";
 
+const App = () => {
+  // Create a reference to store the value entered by the user
+  const searchInput = useRef(null);
   return (
-    <div>Welcome to Unsplash Image Search</div>
-  )
-}
+    <div className="container">
+      <h1 className="title">Image Search</h1>
+      <div className="search-section">
+        <Form>
+          <Form.Control
+            type="search"
+            placeholder="Type something to search..."
+            className="search-input"
+            ref={searchInput}
+          />
+        </Form>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
