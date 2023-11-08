81d96293a3498409e4b6039a567845d04a2df8bb
milliorn <scottmilliorn@gmail.com>
2023-11-07 19:59:11 -0800

    Handle the Form Submit Action

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useRef } from "react";
+import { FormEvent, useRef } from "react";
 import { Form } from "react-bootstrap";
 
 const App = () => {
-  // Create a reference to store the value entered by the user
-  const searchInput = useRef(null);
+  // Create a reference with the type HTMLInputElement or null
+  const searchInput = useRef<HTMLInputElement | null>(null);
+
+  // Handle the search input change event
+  const handleInputChange = (event: FormEvent) => {
+    event.preventDefault(); // Stop the form from submitting
+    console.log("Submitted value: ", searchInput.current?.value); // Log the submitted value
+  };
+
   return (
     <div className="container">
       <h1 className="title">Image Search</h1>
       <div className="search-section">
-        <Form>
+        <Form onSubmit={handleInputChange}>
           <Form.Control
             type="search"
             placeholder="Type something to search..."
